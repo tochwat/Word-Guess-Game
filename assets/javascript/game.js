727e84bd5c0d8fbbfae216a1b3bcaef3cdd9b456
tochwat <tochwat@gmail.com>
2018-12-27 19:26:23 -0800

    first working version without prompts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,13 @@
 
+// Variables to hold references to HTML where we want to display content
+var startText = document.getElementById("start-text");
+var winsText = document.getElementById("wins-text");
+var currentWordText = document.getElementById("current-word-text");
+var remainingGuessesText = document.getElementById("remaining-guesses-text");
+var lettersGuessedText = document.getElementById("letters-guessed-text");
+
+
+
 //Create an array of the possible words for the game to use 
 var words = [
     "banana",
@@ -21,25 +30,42 @@ for (var i=0; i<word.length; i++){
     playerProgress[i] = "_";
 }
 
+//Create player's progress display
+currentWordText.textContent = playerProgress.join(" ");
+
 //Create variable integer to keep track of the length of the randomWord to count down as the player guesses correct letters - when it reaches 0 the player has won
 var remainingLetters = word.length;
 
 
+document.onkeyup = function(event) {
+    var letter = event.key;
+    for (var j=0; j<word.length; j++) {
+        if (word[j]===letter) {
+            playerProgress[j]=letter;
+            remainingLetters--;
+        }
+    }
+
+
+//Update the player's progress display
+currentWordText.textContent = playerProgress.join(" ");
+}
+
+
+
+
+
+/* This all needs to be deleted - replaced the while loop with the event listener
 //While the player hasn't finished guessing the word (remainingLetters is greater than 0)
 while (remainingLetters > 0) {
     //Display the player's progress
-    alert(playerProgress.join(" "));
+    currentWordText.textContent = playerProgress.join(" ");
 
     //take the player's input
-    //Check if the player's guess is valid letter input - if not, tell them to input a single letter
-    //If the player wants to quit - quit the game
     //if they correctly guess a letter, update the progress display and subtract 1 from the remainingLetters variable
-    var letter = prompt("Enter a letter. Click cancel to quit.");
-    if (letter === null) {
-        break;
-    } else if (letter.length !== 1) {
-        alert ("Please enter 1 letter.");
-    } else {
+    
+    document.onkeyup = function(event) {
+        var letter = event.key;
         for (var j=0; j<word.length; j++) {
             if (word[j]===letter) {
                 playerProgress[j]=letter;
@@ -47,11 +73,9 @@ while (remainingLetters > 0) {
             }
         }
     }
-
-
 }
-
+*/
 
 //Congratulate player on winning 
-alert(playerProgress.join(" "));
-alert("Congrats! The word was " + word + "!");
+//alert(playerProgress.join(" "));
+//alert("Congrats! The word was " + word + "!");
