495c2619562ec181980a4f1feee5860b4e235555
tochwat <tochwat@gmail.com>
2018-12-28 19:25:51 -0800

    mostly complete javascript complete, wins and guessesremaining logic is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 
 // Variables to hold references to HTML where we want to display content
 var startText = document.getElementById("start-text");
+var wonText = document.getElementById("won-text");
 var winsText = document.getElementById("wins-text");
 var currentWordText = document.getElementById("current-word-text");
 var remainingGuessesText = document.getElementById("remaining-guesses-text");
@@ -10,13 +11,13 @@ var lettersGuessedText = document.getElementById("letters-guessed-text");
 
 //Create an array of the possible words for the game to use 
 var words = [
-    "banana",
-    "apple",
-//    "grapefruit",
-//    "grapes",
-//    "eggplant",
-//    "cabbage",
-//    "orange"
+    "rainier",
+    "adams",
+//    "baker",
+//    "tahoma",
+//    "kulshan",
+//    "helens",
+//    "bonanza"
 ];
 
 
@@ -30,15 +31,28 @@ for (var i=0; i<word.length; i++){
     playerProgress[i] = "_";
 }
 
-//Create player's progress display
-currentWordText.textContent = playerProgress.join(" ");
 
+
+//Create variable to track remaining guesses
+var guessesRemaining = 15;
 //Create variable integer to keep track of the length of the randomWord to count down as the player guesses correct letters - when it reaches 0 the player has won
 var remainingLetters = word.length;
+//Create variable to keep track of wins
+var wins = 0;
+
+
+//Create wins display
+winsText.textContent = wins;
+//Create player's progress display
+currentWordText.textContent = playerProgress.join(" ");
+//Create the remaining guesses display
+remainingGuessesText.textContent = guessesRemaining;
+
 
 
 document.onkeyup = function(event) {
     var letter = event.key;
+    guessesRemaining--;
     for (var j=0; j<word.length; j++) {
         if (word[j]===letter) {
             playerProgress[j]=letter;
@@ -47,8 +61,53 @@ document.onkeyup = function(event) {
     }
 
 
-//Update the player's progress display
-currentWordText.textContent = playerProgress.join(" ");
+
+    //if remainingLetters is zero, pick a new word and add 1 to numWins, pick a new word and start over 
+    if (remainingLetters===0){
+        //Update won-text
+        wonText.textContent = word;
+
+        //Pick new word from the words array
+        word = words[Math.floor(Math.random() * words.length)];
+
+        //Empty playerProgress array
+        playerProgress = [];
+        //For loop - for length of word, set playerProgress[i] as "_" 
+        for (var i=0; i<word.length; i++){
+            playerProgress[i] = "_";
+        }
+        //Reset number of guesses
+        guessesRemaining = 15;
+        //reset remainingLetters variable for new word
+        remainingLetters = word.length;
+        //add 1 to wins
+        wins++;
+    }
+
+
+    //if guessesRemaining is zero, pick a new word and start over
+    if (guessesRemaining===0){
+        //Pick new word from the words array
+        word = words[Math.floor(Math.random() * words.length)];
+
+        //Empty playerProgress array
+        playerProgress = [];
+        //For loop - for length of word, set playerProgress[i] as "_" 
+        for (var i=0; i<word.length; i++){
+            playerProgress[i] = "_";
+        }
+        //Reset number of guesses
+        guessesRemaining = 15;
+        //reset remainingLetters variable for new word
+        remainingLetters = word.length;
+    }
+
+    //Update wins display
+    winsText.textContent = wins;
+    //Update the player's progress display
+    currentWordText.textContent = playerProgress.join(" ");
+    //Update the remaining guesses display
+    remainingGuessesText.textContent = guessesRemaining;
 }
 
 
